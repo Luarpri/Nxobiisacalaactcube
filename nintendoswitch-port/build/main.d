4c901af47b75a9ca510b241f0734148c88b8eeec
Luarpri <149323701+Luarpri@users.noreply.github.com>
2025-10-03 20:27:43 +0200

    Switch Port!

---
+++ nintendoswitch-port/build/main.d
@@ -0,0 +1,110 @@
+main.o: \
+ D:/devkitPro/examples/switch/graphics/sdl2/sdl2-demo/source/main.c \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_main.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_stdinc.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_config.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_platform.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/begin_code.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/close_code.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_assert.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_atomic.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_audio.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_error.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_endian.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_mutex.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_thread.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_rwops.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_clipboard.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_cpuinfo.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_events.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_video.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_pixels.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_rect.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_surface.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_blendmode.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_keyboard.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_keycode.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_scancode.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_mouse.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_joystick.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_guid.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_gamecontroller.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_sensor.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_quit.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_gesture.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_touch.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_filesystem.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_haptic.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_hidapi.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_hints.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_loadso.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_log.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_messagebox.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_metal.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_power.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_render.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_shape.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_system.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_timer.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_version.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_locale.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_misc.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_mixer.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_image.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL.h \
+ D:/devkitPro/portlibs/switch/include/SDL2/SDL_ttf.h
+D:/devkitPro/portlibs/switch/include/SDL2/SDL.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_main.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_stdinc.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_config.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_platform.h:
+D:/devkitPro/portlibs/switch/include/SDL2/begin_code.h:
+D:/devkitPro/portlibs/switch/include/SDL2/close_code.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_assert.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_atomic.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_audio.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_error.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_endian.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_mutex.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_thread.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_rwops.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_clipboard.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_cpuinfo.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_events.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_video.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_pixels.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_rect.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_surface.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_blendmode.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_keyboard.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_keycode.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_scancode.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_mouse.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_joystick.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_guid.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_gamecontroller.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_sensor.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_quit.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_gesture.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_touch.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_filesystem.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_haptic.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_hidapi.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_hints.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_loadso.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_log.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_messagebox.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_metal.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_power.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_render.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_shape.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_system.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_timer.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_version.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_locale.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_misc.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_mixer.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_image.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL.h:
+D:/devkitPro/portlibs/switch/include/SDL2/SDL_ttf.h:
