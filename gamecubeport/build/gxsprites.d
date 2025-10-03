1b9435e96b24eb0b1e362b38b882e6c0b9aaa029
Luarpri <149323701+Luarpri@users.noreply.github.com>
2025-10-03 19:43:04 +0200

    Add files via upload

---
+++ gamecubeport/build/gxsprites.d
@@ -0,0 +1,92 @@
+gxsprites.o: \
+ D:/devkitPro/examples/gamecube/graphics/gx/gxSprites/source/gxsprites.c \
+ D:/devkitPro/libogc/include/gccore.h \
+ D:/devkitPro/libogc/include/ogc/dsp.h \
+ D:/devkitPro/libogc/include/gctypes.h \
+ D:/devkitPro/libogc/include/ogc/aram.h \
+ D:/devkitPro/libogc/include/gcbool.h \
+ D:/devkitPro/libogc/include/ogc/arqueue.h \
+ D:/devkitPro/libogc/include/ogc/lwp_queue.h \
+ D:/devkitPro/libogc/include/ogc/aram.h \
+ D:/devkitPro/libogc/include/ogc/arqmgr.h \
+ D:/devkitPro/libogc/include/ogc/audio.h \
+ D:/devkitPro/libogc/include/ogc/cache.h \
+ D:/devkitPro/libogc/include/ogc/card.h \
+ D:/devkitPro/libogc/include/ogc/cast.h \
+ D:/devkitPro/libogc/include/ogc/color.h \
+ D:/devkitPro/libogc/include/ogc/consol.h \
+ D:/devkitPro/libogc/include/ogc/console.h \
+ D:/devkitPro/libogc/include/ogc/gx_struct.h \
+ D:/devkitPro/libogc/include/ogc/dvd.h \
+ D:/devkitPro/libogc/include/ogc/disc_io.h \
+ D:/devkitPro/libogc/include/ogc/exi.h \
+ D:/devkitPro/libogc/include/ogc/gu.h \
+ D:/devkitPro/libogc/include/ogc/gx.h \
+ D:/devkitPro/libogc/include/ogc/lwp.h \
+ D:/devkitPro/libogc/include/ogc/gu.h \
+ D:/devkitPro/libogc/include/ogc/si.h \
+ D:/devkitPro/libogc/include/ogc/gx_struct.h \
+ D:/devkitPro/libogc/include/ogc/irq.h \
+ D:/devkitPro/libogc/include/ogc/context.h \
+ D:/devkitPro/libogc/include/ogc/lwp.h \
+ D:/devkitPro/libogc/include/ogc/mutex.h \
+ D:/devkitPro/libogc/include/ogc/message.h \
+ D:/devkitPro/libogc/include/ogc/semaphore.h \
+ D:/devkitPro/libogc/include/ogc/pad.h \
+ D:/devkitPro/libogc/include/ogc/tpl.h \
+ D:/devkitPro/libogc/include/ogc/gx.h \
+ D:/devkitPro/libogc/include/ogc/system.h \
+ D:/devkitPro/libogc/include/gcutil.h \
+ D:/devkitPro/libogc/include/ogc/video.h \
+ D:/devkitPro/libogc/include/ogc/video_types.h \
+ D:/devkitPro/libogc/include/ogc/usbgecko.h \
+ D:/devkitPro/libogc/include/ogc/video_types.h \
+ D:/devkitPro/libogc/include/ogc/texconv.h \
+ D:/devkitPro/libogc/include/ogc/tpl.h \
+ D:/devkitPro/examples/gamecube/graphics/gx/gxSprites/build/textures_tpl.h \
+ D:/devkitPro/examples/gamecube/graphics/gx/gxSprites/build/textures.h
+D:/devkitPro/libogc/include/gccore.h:
+D:/devkitPro/libogc/include/ogc/dsp.h:
+D:/devkitPro/libogc/include/gctypes.h:
+D:/devkitPro/libogc/include/ogc/aram.h:
+D:/devkitPro/libogc/include/gcbool.h:
+D:/devkitPro/libogc/include/ogc/arqueue.h:
+D:/devkitPro/libogc/include/ogc/lwp_queue.h:
+D:/devkitPro/libogc/include/ogc/aram.h:
+D:/devkitPro/libogc/include/ogc/arqmgr.h:
+D:/devkitPro/libogc/include/ogc/audio.h:
+D:/devkitPro/libogc/include/ogc/cache.h:
+D:/devkitPro/libogc/include/ogc/card.h:
+D:/devkitPro/libogc/include/ogc/cast.h:
+D:/devkitPro/libogc/include/ogc/color.h:
+D:/devkitPro/libogc/include/ogc/consol.h:
+D:/devkitPro/libogc/include/ogc/console.h:
+D:/devkitPro/libogc/include/ogc/gx_struct.h:
+D:/devkitPro/libogc/include/ogc/dvd.h:
+D:/devkitPro/libogc/include/ogc/disc_io.h:
+D:/devkitPro/libogc/include/ogc/exi.h:
+D:/devkitPro/libogc/include/ogc/gu.h:
+D:/devkitPro/libogc/include/ogc/gx.h:
+D:/devkitPro/libogc/include/ogc/lwp.h:
+D:/devkitPro/libogc/include/ogc/gu.h:
+D:/devkitPro/libogc/include/ogc/si.h:
+D:/devkitPro/libogc/include/ogc/gx_struct.h:
+D:/devkitPro/libogc/include/ogc/irq.h:
+D:/devkitPro/libogc/include/ogc/context.h:
+D:/devkitPro/libogc/include/ogc/lwp.h:
+D:/devkitPro/libogc/include/ogc/mutex.h:
+D:/devkitPro/libogc/include/ogc/message.h:
+D:/devkitPro/libogc/include/ogc/semaphore.h:
+D:/devkitPro/libogc/include/ogc/pad.h:
+D:/devkitPro/libogc/include/ogc/tpl.h:
+D:/devkitPro/libogc/include/ogc/gx.h:
+D:/devkitPro/libogc/include/ogc/system.h:
+D:/devkitPro/libogc/include/gcutil.h:
+D:/devkitPro/libogc/include/ogc/video.h:
+D:/devkitPro/libogc/include/ogc/video_types.h:
+D:/devkitPro/libogc/include/ogc/usbgecko.h:
+D:/devkitPro/libogc/include/ogc/video_types.h:
+D:/devkitPro/libogc/include/ogc/texconv.h:
+D:/devkitPro/libogc/include/ogc/tpl.h:
+D:/devkitPro/examples/gamecube/graphics/gx/gxSprites/build/textures_tpl.h:
+D:/devkitPro/examples/gamecube/graphics/gx/gxSprites/build/textures.h:
---
+++ gamecubeport/build/textures.d
@@ -0,0 +1,3 @@
+textures.tpl: \
+ D:/devkitPro/examples/gamecube/graphics/gx/gxSprites/textures/textures.scf \
+  D:/devkitPro/examples/gamecube/graphics/gx/gxSprites/textures/ballsprites.png 
