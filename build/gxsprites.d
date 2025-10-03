c8e8666e9e440d3699dbe277564aa3529fa19c5f
Luarpri <149323701+Luarpri@users.noreply.github.com>
2025-10-03 18:38:44 +0200

    Add files via upload

---
+++ build/gxsprites.d
@@ -0,0 +1,126 @@
+gxsprites.o: \
+ D:/devkitPro/examples/wii/graphics/gx/gxSprites/source/gxsprites.c \
+ D:/devkitPro/libogc/include/gccore.h \
+ D:/devkitPro/libogc/include/ogc/dsp.h \
+ D:/devkitPro/libogc/include/gctypes.h \
+ D:/devkitPro/libogc/include/ogc/aram.h \
+ D:/devkitPro/libogc/include/gcbool.h \
+ D:/devkitPro/libogc/include/ogc/arqueue.h \
+ D:/devkitPro/libogc/include/ogc/lwp_queue.h \
+ D:/devkitPro/libogc/include/ogc/aram.h \
+ D:/devkitPro/libogc/include/ogc/arqmgr.h \
+ D:/devkitPro/libogc/include/ogc/audio.h \
+ D:/devkitPro/libogc/include/ogc/cache.h \
+ D:/devkitPro/libogc/include/ogc/card.h \
+ D:/devkitPro/libogc/include/ogc/cast.h \
+ D:/devkitPro/libogc/include/ogc/color.h \
+ D:/devkitPro/libogc/include/ogc/consol.h \
+ D:/devkitPro/libogc/include/ogc/console.h \
+ D:/devkitPro/libogc/include/ogc/gx_struct.h \
+ D:/devkitPro/libogc/include/ogc/dvd.h \
+ D:/devkitPro/libogc/include/ogc/disc_io.h \
+ D:/devkitPro/libogc/include/ogc/exi.h \
+ D:/devkitPro/libogc/include/ogc/gu.h \
+ D:/devkitPro/libogc/include/ogc/gx.h \
+ D:/devkitPro/libogc/include/ogc/lwp.h \
+ D:/devkitPro/libogc/include/ogc/gu.h \
+ D:/devkitPro/libogc/include/ogc/si.h \
+ D:/devkitPro/libogc/include/ogc/gx_struct.h \
+ D:/devkitPro/libogc/include/ogc/irq.h \
+ D:/devkitPro/libogc/include/ogc/context.h \
+ D:/devkitPro/libogc/include/ogc/lwp.h \
+ D:/devkitPro/libogc/include/ogc/mutex.h \
+ D:/devkitPro/libogc/include/ogc/message.h \
+ D:/devkitPro/libogc/include/ogc/semaphore.h \
+ D:/devkitPro/libogc/include/ogc/pad.h \
+ D:/devkitPro/libogc/include/ogc/tpl.h \
+ D:/devkitPro/libogc/include/ogc/gx.h \
+ D:/devkitPro/libogc/include/ogc/system.h \
+ D:/devkitPro/libogc/include/gcutil.h \
+ D:/devkitPro/libogc/include/ogc/video.h \
+ D:/devkitPro/libogc/include/ogc/video_types.h \
+ D:/devkitPro/libogc/include/ogc/usbgecko.h \
+ D:/devkitPro/libogc/include/ogc/video_types.h \
+ D:/devkitPro/libogc/include/ogc/texconv.h \
+ D:/devkitPro/libogc/include/ogc/ipc.h \
+ D:/devkitPro/libogc/include/ogc/sha.h \
+ D:/devkitPro/libogc/include/ogc/aes.h \
+ D:/devkitPro/libogc/include/ogc/es.h \
+ D:/devkitPro/libogc/include/ogc/stm.h \
+ D:/devkitPro/libogc/include/ogc/ios.h \
+ D:/devkitPro/libogc/include/ogc/usb.h \
+ D:/devkitPro/libogc/include/ogc/isfs.h \
+ D:/devkitPro/libogc/include/ogc/conf.h \
+ D:/devkitPro/libogc/include/ogc/usbstorage.h \
+ D:/devkitPro/libogc/include/ogc/mutex.h \
+ D:/devkitPro/libogc/include/ogc/system.h \
+ D:/devkitPro/libogc/include/ogc/wiilaunch.h \
+ D:/devkitPro/libogc/include/wiiuse/wpad.h \
+ D:/devkitPro/libogc/include/wiiuse/wiiuse.h \
+ D:/devkitPro/libogc/include/bte/bte.h \
+ D:/devkitPro/libogc/include/bte/bd_addr.h \
+ D:/devkitPro/libogc/include/ogc/tpl.h \
+ D:/devkitPro/examples/wii/graphics/gx/gxSprites/build/textures_tpl.h \
+ D:/devkitPro/examples/wii/graphics/gx/gxSprites/build/textures.h
+D:/devkitPro/libogc/include/gccore.h:
+D:/devkitPro/libogc/include/ogc/dsp.h:
+D:/devkitPro/libogc/include/gctypes.h:
+D:/devkitPro/libogc/include/ogc/aram.h:
+D:/devkitPro/libogc/include/gcbool.h:
+D:/devkitPro/libogc/include/ogc/arqueue.h:
+D:/devkitPro/libogc/include/ogc/lwp_queue.h:
+D:/devkitPro/libogc/include/ogc/aram.h:
+D:/devkitPro/libogc/include/ogc/arqmgr.h:
+D:/devkitPro/libogc/include/ogc/audio.h:
+D:/devkitPro/libogc/include/ogc/cache.h:
+D:/devkitPro/libogc/include/ogc/card.h:
+D:/devkitPro/libogc/include/ogc/cast.h:
+D:/devkitPro/libogc/include/ogc/color.h:
+D:/devkitPro/libogc/include/ogc/consol.h:
+D:/devkitPro/libogc/include/ogc/console.h:
+D:/devkitPro/libogc/include/ogc/gx_struct.h:
+D:/devkitPro/libogc/include/ogc/dvd.h:
+D:/devkitPro/libogc/include/ogc/disc_io.h:
+D:/devkitPro/libogc/include/ogc/exi.h:
+D:/devkitPro/libogc/include/ogc/gu.h:
+D:/devkitPro/libogc/include/ogc/gx.h:
+D:/devkitPro/libogc/include/ogc/lwp.h:
+D:/devkitPro/libogc/include/ogc/gu.h:
+D:/devkitPro/libogc/include/ogc/si.h:
+D:/devkitPro/libogc/include/ogc/gx_struct.h:
+D:/devkitPro/libogc/include/ogc/irq.h:
+D:/devkitPro/libogc/include/ogc/context.h:
+D:/devkitPro/libogc/include/ogc/lwp.h:
+D:/devkitPro/libogc/include/ogc/mutex.h:
+D:/devkitPro/libogc/include/ogc/message.h:
+D:/devkitPro/libogc/include/ogc/semaphore.h:
+D:/devkitPro/libogc/include/ogc/pad.h:
+D:/devkitPro/libogc/include/ogc/tpl.h:
+D:/devkitPro/libogc/include/ogc/gx.h:
+D:/devkitPro/libogc/include/ogc/system.h:
+D:/devkitPro/libogc/include/gcutil.h:
+D:/devkitPro/libogc/include/ogc/video.h:
+D:/devkitPro/libogc/include/ogc/video_types.h:
+D:/devkitPro/libogc/include/ogc/usbgecko.h:
+D:/devkitPro/libogc/include/ogc/video_types.h:
+D:/devkitPro/libogc/include/ogc/texconv.h:
+D:/devkitPro/libogc/include/ogc/ipc.h:
+D:/devkitPro/libogc/include/ogc/sha.h:
+D:/devkitPro/libogc/include/ogc/aes.h:
+D:/devkitPro/libogc/include/ogc/es.h:
+D:/devkitPro/libogc/include/ogc/stm.h:
+D:/devkitPro/libogc/include/ogc/ios.h:
+D:/devkitPro/libogc/include/ogc/usb.h:
+D:/devkitPro/libogc/include/ogc/isfs.h:
+D:/devkitPro/libogc/include/ogc/conf.h:
+D:/devkitPro/libogc/include/ogc/usbstorage.h:
+D:/devkitPro/libogc/include/ogc/mutex.h:
+D:/devkitPro/libogc/include/ogc/system.h:
+D:/devkitPro/libogc/include/ogc/wiilaunch.h:
+D:/devkitPro/libogc/include/wiiuse/wpad.h:
+D:/devkitPro/libogc/include/wiiuse/wiiuse.h:
+D:/devkitPro/libogc/include/bte/bte.h:
+D:/devkitPro/libogc/include/bte/bd_addr.h:
+D:/devkitPro/libogc/include/ogc/tpl.h:
+D:/devkitPro/examples/wii/graphics/gx/gxSprites/build/textures_tpl.h:
+D:/devkitPro/examples/wii/graphics/gx/gxSprites/build/textures.h:
---
+++ build/textures.d
@@ -0,0 +1,3 @@
+textures.tpl: \
+ D:/devkitPro/examples/wii/graphics/gx/gxSprites/textures/textures.scf \
+  D:/devkitPro/examples/wii/graphics/gx/gxSprites/textures/ballsprites.png 
